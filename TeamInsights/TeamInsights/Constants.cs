f0ce626b98323acb63425066787d4318a8b7f10f
Ajaykumar <ajaykumarkaranki1@gmail.com>
2025-02-03 18:50:01 -0600

    Team Insights Template Ajay-Sprint1

---
+++ TeamInsights/TeamInsights/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace TeamInsights
+{
+    public class Constants
+    {
+        public const string AdministratorRole = "Administrator";
+        public const string StandardUserRole = "StandardUser";
+        public const string CustomerSessionKey = "Customer";
+    }
+}
