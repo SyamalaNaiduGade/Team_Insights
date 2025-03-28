f8fbfd32a1fb0cfd3538fa3b1a8942c2a413e1b3
Erica Chambers <53728223+ebc1201@users.noreply.github.com>
2025-03-28 12:25:51 -0500

    Frontend changes for sprint 3. Managers/Employees cards

--- TeamInsights/TeamInsights/obj/Debug/net8.0/TeamInsights.AssemblyInfo.cs
+++ TeamInsights/TeamInsights/obj/Debug/net8.0/TeamInsights.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeamInsights")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf0317f1b0b53f33cbb551966860985d45c11503")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0459fe543dbb26de4806ae1fe5e6f991dec06d15")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeamInsights")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeamInsights")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
