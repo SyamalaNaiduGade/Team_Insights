8d735c0435e95e8c1cc8eaf82cf8cff6c163d094
Syamala Naidu gade <syamalanaidugade56@gmail.com>
2025-04-07 18:54:47 -0500

    Added Top 5 performers

---
+++ TeamInsights/TeamInsights/ViewModels/RoleDistributionViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TeamInsights.ViewModels
+{
+    public class RoleDistributionViewModel
+    {
+        public string RoleName { get; set; }
+        public int EmployeeCount { get; set; }
+        public double Percentage { get; set; }
+    }
+}
